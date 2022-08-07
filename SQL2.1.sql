2e57e6b0341cc5d2caf5927646e3c6c920a97e7d
Alexander Filippov <sash.f@rambler.ru>
2022-08-07 15:46:50 +0300

    BD_SQL

--- SQL2.1.sql
+++ SQL2.1.sql
@@ -3,51 +3,48 @@
 
 CREATE TABLE IF NOT EXISTS Musical_genres (
 	id SERIAL PRIMARY KEY,
-	name_genre VARCHAR(60) NOT NULL,
+	name_genre VARCHAR(60) UNIQUE NOT NULL,
 	description TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Performers (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(60) NOT NULL,
-	genre VARCHAR(60) NOT NULL,
 	description TEXT 
 );
 
 CREATE TABLE IF NOT EXISTS Performers_Musical_genres (
 	id_The_performer INTEGER REFERENCES Performers(id),
-	ID_Music_genre INTEGER REFERENCES Musical_genres(id),
-	CONSTRAINT pk PRIMARY KEY (ID_The_performer, ID_Music_genre)
+	id_Music_genre INTEGER REFERENCES Musical_genres(id),
+	CONSTRAINT pk PRIMARY KEY (id_The_performer, id_Music_genre)
 );
 
 CREATE TABLE IF NOT EXISTS Albums (
 	id SERIAL PRIMARY KEY,
 	title VARCHAR(60) NOT NULL,
-	year_release INTEGER NOT NULL,
-	executor VARCHAR(60) NOT NULL,
+	year_release INTEGER CHECK(year_release > 1990) NOT NULL,
 	description TEXT 
 );
 
 CREATE TABLE IF NOT EXISTS Performers_Albums (
-	ID_The_performer INTEGER REFERENCES Performers(id),
-	ID_Albums INTEGER REFERENCES Albums(id),
-	CONSTRAINT pk1 PRIMARY KEY (ID_The_performer, ID_Albums)
+	id_The_performer INTEGER REFERENCES Performers(id),
+	id_Albums INTEGER REFERENCES Albums(id),
+	CONSTRAINT pk1 PRIMARY KEY (id_The_performer, id_Albums)
 );
 
 CREATE TABLE IF NOT EXISTS Tracks (
 	id SERIAL PRIMARY KEY,
-	ID_Albums INTEGER NOT NULL REFERENCES Albums(id),
+	id_Albums INTEGER NOT NULL REFERENCES Albums(id),
 	title VARCHAR(60) NOT NULL,
-	duration INTEGER NOT NULL,
-	album VARCHAR(60) NOT NULL,
+	duration FLOAT CHECK(duration > 0) NOT NULL,
 	description TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Collection (
 	id SERIAL PRIMARY KEY,
-	title VARCHAR(60) NOT NULL,
-	description TEXT,
-	year_release INTEGER NOT NULL
+	title VARCHAR(60) UNIQUE NOT NULL,
+	year_release INTEGER CHECK(year_release > 1990) NOT NULL,
+	description TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Tracks_Collection (
--- SQL2.2.sql
+++ SQL2.2.sql
@@ -1,18 +1,11 @@
 #Дополнительное (необязательное) задание
 
-CREATE TABLE IF NOT EXISTS Chief (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(60) NOT NULL,
-	department VARCHAR(60) NOT NULL,
-	description TEXT
-);
-
 CREATE TABLE IF NOT EXISTS Employee (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(60) NOT NULL,
 	department VARCHAR(60) NOT NULL,
-	ID_The_boss INTEGER NOT NULL REFERENCES Chief(id),
-	id INTEGER PRIMARY KEY REFERENCES Chief(id),
+	Chief VARCHAR(60) NOT NULL,
+	id_Chief INTEGER NOT NULL REFERENCES Employee(id),
 	description TEXT
 );
 
